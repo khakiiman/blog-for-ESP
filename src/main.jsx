3afa73ae720878e4fccac1040b47db62b1b1fa8b
khakiiman <ikhaki71@gmail.com>
2023-08-06 20:08:58 +0330

    Removing Unused Files, Initializing Setup and Project Structure, Installing [dev]Dependencies, Add TailwindCSS to Vite App, Configuring Basic Settings.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// Packages
+import ReactDOM from 'react-dom/client';
 
+// Files
+import App from './App.jsx';
+
+// Styles
+
+// Code
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <>
     <App />
-  </React.StrictMode>,
-)
+  </>
+);
